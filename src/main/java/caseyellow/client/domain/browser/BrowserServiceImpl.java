e3be239bd6b56baa396bd8793ab7fac3074372ab
Dan <dangolan24@gmail.com>
2017-12-25 20:09:17 +0200

    remove selnuim testing message

--- src/main/java/caseyellow/client/domain/browser/BrowserServiceImpl.java
+++ src/main/java/caseyellow/client/domain/browser/BrowserServiceImpl.java
@@ -84,7 +84,7 @@ public class BrowserServiceImpl implements BrowserService {
         logPath = new File(Utils.createTmpDir(), "log_net").getAbsolutePath();
         String log_flag = "--log-net-log=" + logPath;
         options.addArguments(log_flag);
-
+        options.addArguments("disable-infobars");
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player", 1);
         options.setExperimentalOption("prefs", prefs);
