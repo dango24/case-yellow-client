98b0e05f52a7971b8ab475680a3e3c2755b5ebfa
Dan <dangolan24@gmail.com>
2018-02-20 11:56:58 +0200

    fix amazon-workSpace hash value and file size

--- src/test/java/caseyellow/client/domain/file/service/DownloadFileServiceImplTest.java
+++ src/test/java/caseyellow/client/domain/file/service/DownloadFileServiceImplTest.java
@@ -2,12 +2,16 @@ package caseyellow.client.domain.file.service;
 
 import caseyellow.client.App;
 import caseyellow.client.domain.file.model.FileDownloadProperties;
+import caseyellow.client.domain.message.MessageServiceImp;
 import caseyellow.client.domain.message.MessagesService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.mock;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
@@ -17,12 +21,15 @@ public class DownloadFileServiceImplTest {
     private DownloadFileServiceImpl downloadFileService;
 
     @Autowired
-    private MessagesService messagesService;
+    @Qualifier("messageServiceImp")
+    private MessageServiceImp messagesService;
 
     @Test
     public void generateFileDownloadInfo() throws Exception {
 
+        MessagesService presentationMessageService = mock(MessagesService.class);
+        messagesService.setPresentationMessagesService(presentationMessageService);
         downloadFileService.setMessagesService(messagesService);
-        downloadFileService.generateFileDownloadInfo(new FileDownloadProperties("vlc", "http://mirror.library.ucy.ac.cy/videolan/vlc/2.2.8/win32/vlc-2.2.8-win32.exe"));
+        downloadFileService.generateFileDownloadInfo(new FileDownloadProperties("amazon-workSpace", "https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi"));
     }
 }
