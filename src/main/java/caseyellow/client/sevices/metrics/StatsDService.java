696daa1abc1408d3c7a2ad2d7e0559ebcb20b921
dango <dango@checkpoint.com>
2018-05-03 15:44:39 +0300

    statsd init

---
+++ src/main/java/caseyellow/client/sevices/metrics/StatsDService.java
@@ -0,0 +1,35 @@
+package caseyellow.client.sevices.metrics;
+
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.stream.IntStream;
+
+@Service
+public class StatsDService {
+
+    @Value("${statsd.prefix}")
+    private String statsDprefix;
+
+    @Value("${statsd.host}")
+    private String statsDHost;
+
+    @Value("${statsd.port:8125}")
+    private int statsDPort;
+
+    private StatsDClient statsDClient;
+
+    @PostConstruct
+    private void init() {
+        statsDClient = new NonBlockingStatsDClient(statsDprefix, statsDHost, statsDPort);
+        statsDClient.incrementCounter("bar");
+        IntStream.range(0, 100).forEach( (i) -> statsDClient.count("dango", 10));
+        statsDClient.recordGaugeValue("baz", 100);
+        statsDClient.count("dango", 10);
+        statsDClient.recordExecutionTime("bag", 25);
+        statsDClient.recordSetEvent("qux", "one");
+    }
+}
