d3bad4e568012f27bef40f26d470532bda0c727b
dangolan <dangolan@amazon.com>
2018-07-29 18:57:17 +0300

    Clean up adn refactor App class

--- src/main/java/caseyellow/client/App.java
+++ src/main/java/caseyellow/client/App.java
@@ -9,6 +9,7 @@ import caseyellow.client.domain.test.service.TestGeneratorImpl;
 import caseyellow.client.domain.message.MessageServiceImp;
 import caseyellow.client.domain.test.service.TestServiceImpl;
 import caseyellow.client.domain.website.service.WebSiteServiceImpl;
+import caseyellow.client.exceptions.LoginException;
 import caseyellow.client.presentation.MainFrameImpl;
 import caseyellow.client.sevices.gateway.model.AccountCredentials;
 import caseyellow.client.sevices.gateway.model.LoginDetails;
@@ -38,13 +39,14 @@ public class App {
     private static MainFrameImpl mainForm;
 
     public static void main(String[] args) throws Exception {
+
         try {
-            if(args.length < 1)
-            {
+            if (args.length == 0) {
                 initView();
             }
 
             initApplicationContext(args);
+
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
@@ -58,62 +60,87 @@ public class App {
     private static void initApplicationContext(String[] args) {
 
         try {
-
             ApplicationContext ctx = SpringApplication.run(App.class, args);
             TestGeneratorImpl testGenerator = (TestGeneratorImpl) ctx.getBean("testGenerator");
             MessageServiceImp messagesService = (MessageServiceImp) ctx.getBean("messageServiceImp");
             GatewayService gatewayService = (GatewayService) ctx.getBean("gatewayService");
 
-            if(args.length > 0) {
-                // configuration file mode
-
-                System.setProperty("java.awt.headless", "false");
-
-                TestServiceImpl testService = (TestServiceImpl)ctx.getBean("testServiceImpl");
-                WebSiteServiceImpl webSiteService = (WebSiteServiceImpl)ctx.getBean("webSiteServiceImpl");
-                DownloadFileServiceImpl downloadFileService = (DownloadFileServiceImpl) ctx.getBean("downloadFileServiceImpl");
-                SystemServiceImpl systemService = (SystemServiceImpl)ctx.getBean("systemServiceImpl");
-
-                CmdLineMessageImpl cmdLineMessage = new CmdLineMessageImpl();
-                testService.setMessagesService(cmdLineMessage);
-                webSiteService.setMessagesService(cmdLineMessage);
-                downloadFileService.setMessagesService(cmdLineMessage);
-                systemService.setMessagesService(cmdLineMessage);
-                testGenerator.setMainFrame(new MainFrameImpl());
-
-                String rawConfiguration = FileUtils.readFile(args[0]);
-                String[] rawFields = rawConfiguration.split("\n");
-                String user = rawFields[0].trim();
-                String password = rawFields[1].trim();
-
-
-
-                LoginDetails loginDetails = gatewayService.login(new AccountCredentials(user, password));
-                if (loginDetails.isSucceed()) {
-                    if (loginDetails.isRegistration()) {
-                        String infra = rawFields[2].trim();
-                        int speed = Integer.valueOf(rawFields[3].trim());
-                        gatewayService.saveConnectionDetails(new ConnectionDetails(infra, speed));
-                    }
-
-                    testGenerator.startProducingTests() ;
-                    return;
-                }
-                else System.out.println("Failed to login, Aborting...");
-                return;
+            if (isCmdLineMode(args)) {
+                executeCmdLineMode(args[0], ctx, testGenerator, gatewayService);
+            } else {
+                executeGUIMode(testGenerator, messagesService, gatewayService);
             }
-            
-            testGenerator.setMainFrame(mainForm);
-            messagesService.setPresentationMessagesService(mainForm);
-            mainForm.setStartProducingTestsCommand(testGenerator);
-            mainForm.setStopProducingTestsCommand(testGenerator);
-            mainForm.setGatewayService(gatewayService);
-            mainForm.enableApp();
+
 
         } catch (Exception e) {
             logger.error(e.getMessage());
             JOptionPane.showMessageDialog(null, "The best app ever failed to initialized, " + e.getMessage());
             mainForm.terminate();
         }
+    }
+
+    private static void executeGUIMode(TestGeneratorImpl testGenerator, MessageServiceImp messagesService, GatewayService gatewayService) {
+        testGenerator.setMainFrame(mainForm);
+        messagesService.setPresentationMessagesService(mainForm);
+        mainForm.setStartProducingTestsCommand(testGenerator);
+        mainForm.setStopProducingTestsCommand(testGenerator);
+        mainForm.setGatewayService(gatewayService);
+        mainForm.enableApp();
+    }
+
+    private static void executeCmdLineMode(String configPath, ApplicationContext ctx, TestGeneratorImpl testGenerator, GatewayService gatewayService) throws IOException, LoginException {
+
+        System.setProperty("java.awt.headless", "false");
+        injectCmdLineMessageService(ctx);
+        testGenerator.setMainFrame(new MainFrameImpl());
+
+        LoginDetails loginDetails = gatewayService.login(createAccountCredentials(configPath));
+
+        if (loginDetails.isSucceed()) {
+
+            if (loginDetails.isRegistration()) {
+                gatewayService.saveConnectionDetails(createConnectionDetails(configPath));
+            }
+
+            testGenerator.startProducingTests() ;
+
+        } else {
+            System.err.println("Failed to login, Aborting...");
+        }
+    }
+
+    private static void injectCmdLineMessageService(ApplicationContext ctx) {
+        TestServiceImpl testService = (TestServiceImpl)ctx.getBean("testServiceImpl");
+        WebSiteServiceImpl webSiteService = (WebSiteServiceImpl)ctx.getBean("webSiteServiceImpl");
+        DownloadFileServiceImpl downloadFileService = (DownloadFileServiceImpl) ctx.getBean("downloadFileServiceImpl");
+        SystemServiceImpl systemService = (SystemServiceImpl)ctx.getBean("systemServiceImpl");
+
+        CmdLineMessageImpl cmdLineMessage = new CmdLineMessageImpl();
+        testService.setMessagesService(cmdLineMessage);
+        webSiteService.setMessagesService(cmdLineMessage);
+        downloadFileService.setMessagesService(cmdLineMessage);
+        systemService.setMessagesService(cmdLineMessage);
+    }
+
+    private static AccountCredentials createAccountCredentials(String configPath) throws IOException {
+        String rawConfiguration = FileUtils.readFile(configPath);
+        String[] rawFields = rawConfiguration.split("\n");
+        String user = rawFields[0].trim();
+        String password = rawFields[1].trim();
+
+        return new AccountCredentials(user, password);
+    }
+
+    private static ConnectionDetails createConnectionDetails(String configPath) throws IOException {
+        String rawConfiguration = FileUtils.readFile(configPath);
+        String[] rawFields = rawConfiguration.split("\n");
+        String infra = rawFields[2].trim();
+        int speed = Integer.valueOf(rawFields[3].trim());
+
+        return new ConnectionDetails(infra, speed);
+    }
+
+    private static boolean isCmdLineMode(String[] args) {
+        return args.length > 0;
     }
 }
